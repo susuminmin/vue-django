1ea33542b18d0eb77a86ad0209fc5e28ca89516d
김수민 <mytoutbon@gmail.com>
2019-11-19 11:12:35 +0900

    Implement jwt auth server

---
+++ todo-back/todos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser 
+
+# User 는 Custom User 를 사용 (default user를 사용하더라도 django 에서는 *강력히* Custom User 를 사용하라고 권장)
+
+class User(AbstractUser):
+    pass
+
+
+class Todo(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
---
+++ todo-front/src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- 3. 컴퍼넌트 사용 -->
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+// 1. 컴퍼넌트 호출
+import LoginForm from '@/components/LoginForm'
+
+export default {
+  name: 'Login',
+  components: {
+    // 2. 컴퍼넌트 등록
+    LoginForm,
+  }
+}
+</script>
+
+<style>
+
+</style>
